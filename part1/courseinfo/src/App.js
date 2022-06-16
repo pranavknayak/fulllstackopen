d23ecee53f06313800d9e2fa1f47d91830eb9511
Pranav <es20btech11035@iith.ac.in>
2022-06-16 21:45:37 +0530

    finished 1.1 and 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.p1} exercises={props.ex1}/>
+      <Part part={props.p2} exercises={props.ex2}/>
+      <Part part={props.p3} exercises={props.ex3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.ex1 + props.ex2 + props.ex3}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return(
+    <div>
+      <Header course={course} />
+      <Content p1={part1} p2={part2} p3={part3} ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
