b39efbf5fd53caa6991bbdc3acf1d9406e0f4934
Pranav <es20btech11035@iith.ac.in>
2022-06-22 18:15:59 +0530

    finished 1.6 - 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -22,10 +22,35 @@ const DisplayStats = ({ text, value }) => {
   )
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  if(good === 0 && bad === 0 && neutral === 0){
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  else{
+    return (
+      <div>
+        <DisplayStats text='good' value={good} />
+        <DisplayStats text='neutral' value={neutral} />
+        <DisplayStats text='bad' value={bad} />
+        <DisplayStats text='all' value={good + bad + neutral} />
+        <DisplayStats text='average' value={(good - bad)/(good + bad + neutral)} />
+        <DisplayStats text='positive' value={good/(good + bad + neutral)} />
+
+      </div>
+    )
+  }
+
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
+
 
 
 
@@ -36,9 +61,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button onClick={() => setBad(bad + 1)} text='bad' />
       <Heading text='statistics' />
-      <DisplayStats text='good' value={good} />
-      <DisplayStats text='neutral' value={neutral} />
-      <DisplayStats text='bad' value={bad} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
 
     </div>
   )
